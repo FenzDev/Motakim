aaeea6c28118384e6fbef71721c7b83dcf2621c5
فنيش منصف <68622361+FennishMoncef@users.noreply.github.com>
2021-12-23 17:46:38 +0100

    Including args into Start method

--- Motakim.Editor.GL/Program.cs
+++ Motakim.Editor.GL/Program.cs
@@ -2,6 +2,6 @@ namespace Motakim.Editor
 {
     static class Program
     {
-        static void Main(string[] args) => EditorProgram.Start();    
+        static void Main(string[] args) => EditorProgram.Start(args);    
     }
 }
