42e561cf4cd023caefe8f8d446bf7ae599d76eb0
فنيش منصف <68622361+FennishMoncef@users.noreply.github.com>
2021-12-30 22:57:00 +0100

    Revert "Getting back to WPF!"
    This reverts commit 4f3d0af00ce6af0beb809dd87e53470cdb1cd3d6.

---
+++ Motakim.Editor.DX/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Motakim.Editor
+{
+    class Program
+    {
+        static void Main(string[] args) => EditorProgram.Start(args);
+    }
+}
---
+++ Motakim.Editor.GL/Program.cs
@@ -0,0 +1,7 @@
+namespace Motakim.Editor
+{
+    static class Program
+    {
+        static void Main(string[] args) => EditorProgram.Start(args);    
+    }
+}
